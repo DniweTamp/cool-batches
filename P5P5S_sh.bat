2878d78aa9e55cc5e3c22bb4c5e0edc875090bf0
DniweTamp <39583006+DniweTamp@users.noreply.github.com>
2022-02-04 20:20:21 +0300

    Update P5P5S_sh.bat

--- P5P5S_sh.bat
+++ P5P5S_sh.bat
@@ -2,7 +2,7 @@
 title P5S Texture Adjustment
 FOR %%a IN (%*) DO (
     echo Adjusting: %%~na%%~xa
-	convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average -level 0%%,100%%,5.7 "%%~pa%%~na_sh.png"
+	convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_sh.png"
 	convert "%%~pa%%~na_sh.png" -fill white -colorize 100 "%%~pa%%~na_wh.png"
 	convert "%%~pa%%~na_sh.png" -fill black -colorize 100 "%%~pa%%~na_bl.png"
 	convert "%%~pa%%~na_bl.png" "%%~pa%%~na_wh.png" "%%~pa%%~na_sh.png" -combine "%%~pa%%~na_p5s.png"
