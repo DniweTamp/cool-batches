50b1688b8f54546156532570be2d984dc46ec564
DniweTamp <39583006+DniweTamp@users.noreply.github.com>
2022-02-09 19:24:05 +0300

    Correct p5 alpha transferring to p5s red

--- P5P5S_sh.bat
+++ P5P5S_sh.bat
@@ -2,11 +2,12 @@
 title P5S Texture Adjustment
 FOR %%a IN (%*) DO (
     echo Adjusting: %%~na%%~xa
-	convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_sh.png"
-	convert "%%~pa%%~na_sh.png" -fill white -colorize 100 "%%~pa%%~na_wh.png"
-	convert "%%~pa%%~na_sh.png" -fill black -colorize 100 "%%~pa%%~na_bl.png"
-	convert "%%~pa%%~na_bl.png" "%%~pa%%~na_wh.png" "%%~pa%%~na_sh.png" -combine "%%~pa%%~na_p5s.png"
-	del "%%~pa%%~na_sh.png"
-	del "%%~pa%%~na_bl.png"
-	del "%%~pa%%~na_wh.png"
+    convert -alpha extract %%a "%%~pa%%~na_alp.png"
+    convert "%%~pa%%~na_alp.png" -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_alp.png"
+    convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_sh.png"
+    convert "%%~pa%%~na_sh.png" -fill white -colorize 100 "%%~pa%%~na_wh.png"
+    convert "%%~pa%%~na_alp.png" "%%~pa%%~na_wh.png" "%%~pa%%~na_sh.png" -combine "%%~pa%%~na_p5s.png"
+    del "%%~pa%%~na_sh.png"
+    del "%%~pa%%~na_alp.png"
+    del "%%~pa%%~na_wh.png"
 )
