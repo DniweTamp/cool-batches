98a517be4412e50638f59a35bcb5dd9e5f46dbc9
DniweTamp <39583006+DniweTamp@users.noreply.github.com>
2022-02-09 21:43:32 +0300

    Update P5P5S_sh.bat

--- P5P5S_sh.bat
+++ P5P5S_sh.bat
@@ -4,7 +4,7 @@ FOR %%a IN (%*) DO (
     echo Adjusting: %%~na%%~xa
     convert -alpha extract %%a "%%~pa%%~na_alp.png"
     convert "%%~pa%%~na_alp.png" -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_alp.png"
-    convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average "%%~pa%%~na_sh.png"
+    convert %%a -channel RGB -negate -alpha off -set colorspace Gray -separate -average -brightness-contrast +50x0 "%%~pa%%~na_sh.png"
     convert "%%~pa%%~na_sh.png" -fill white -colorize 100 "%%~pa%%~na_wh.png"
     convert "%%~pa%%~na_alp.png" "%%~pa%%~na_wh.png" "%%~pa%%~na_sh.png" -combine "%%~pa%%~na_p5s.png"
     del "%%~pa%%~na_sh.png"
