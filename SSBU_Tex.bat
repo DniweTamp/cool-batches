62ecc7a4da72e5ac62603b287124dbf952d04222
DniweTamp <39583006+DniweTamp@users.noreply.github.com>
2022-03-01 21:55:12 +0300

    nuked unnecessary alpha trolling

--- SSBU_Tex.bat
+++ SSBU_Tex.bat
@@ -2,9 +2,6 @@
 title Smash Texture Adjustment
 FOR %%a IN (%*) DO (
     echo Adjusting: %%~na%%~xa
-	convert -alpha extract %%a "%%~pa%%~na_alpha.png"
-    magick %%a +level 1,72.16%% "%%~da%%~pa%%~na_col.png"
-	magick "%%~da%%~pa%%~na_col.png" -level 0%%,100%%,1.385  "%%~da%%~pa%%~na_col.png"
-	magick "%%~da%%~pa%%~na_col.png" "%%~pa%%~na_alpha.png" -alpha off -compose CopyOpacity -composite "%%~da%%~pa%%~na_col.png"
-	del "%%~pa%%~na_alpha.png"
+    magick %%a -channel RGB +level 1,72.16%% "%%~da%%~pa%%~na_col.png"
+    magick "%%~da%%~pa%%~na_col.png" -channel RGB -level 0%%,100%%,1.385  "%%~da%%~pa%%~na_col.png"
 )
